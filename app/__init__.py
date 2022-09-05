7afce8131b9df60c6841025a4b70e72761afadb4
Allyson Phillips <61166522+CloudF1sh@users.noreply.github.com>
2022-09-05 11:09:18 -0700

    Made a few tweaks
    Made it a little more visually appealing and added timezone adjustments.  STILL getting the 405 error.

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,10 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+
 from flask_bootstrap import Bootstrap
+
+from flask_moment import Moment
 
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -22,6 +25,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 bootstrap = Bootstrap(app)
+
+moment = Moment(app) 
 
 # At this time, Email alerts are NOT supported.  I plan to se it up in the next iteration.
 if not app.debug:
